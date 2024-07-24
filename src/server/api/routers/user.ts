20a614fda0373fef418da003d119d7cde450dbb9
Ash Gharibyan <ashghcode@gmail.com>
2024-07-24 15:22:42 -0700

    added bcrypt

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -1,6 +1,7 @@
 import { db } from "@/server/db";
 import { createTRPCRouter, publicProcedure } from "../trpc";
 import { UserCreateInputSchema } from "prisma/generated/zod";
+import { hashPassword } from "@/util/hashing";
 
 export const userRouter = createTRPCRouter({
   create: publicProcedure
@@ -9,10 +10,12 @@ export const userRouter = createTRPCRouter({
       console.log("---------input", input);
       const { email, password, username, first_name, last_name } = input;
 
+      const hashedPassword = await hashPassword(password);
+
       await db.user.create({
         data: {
           email,
-          password,
+          password: hashedPassword,
           username,
           first_name,
           last_name,
